1c8e14d0244fd084eccc598b1126483e6562becf
RintaroTawara <rintarou.54@gmail.com>
2021-01-10 16:34:30 +0900

    go-turorial

---
+++ go-restful-api-tutorial/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+  "encoding/json"
+  "fmt"
+  "github.com/gorilla/mux"
+  "log"
+  "net/http"
+)
+
+type Article struct {
+  Title string `json:"Title"`
+  Desc string `json:"desc"`
+  Content string `json:"content"`
+}
+
+type Articles []Article
+
+func testPostArticles(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Test POST endpoint worked")
+}
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+  articles := Articles{
+    Article{Title:"Test Title", Desc: "Test Description", Content: "Hello World"},
+  }
+  fmt.Println("Endpoint Hit: All Articles Endpoint")
+  json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request)  {
+  fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handleRequests() {
+  myRouter := mux.NewRouter().StrictSlash(true)
+  myRouter.HandleFunc("/", homePage)
+  myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+  myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
+  log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
+
+func main() {
+  handleRequests()
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module gosample
+
+go 1.15
+
+require (
+	github.com/aws/aws-lambda-go v1.22.0
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+)
